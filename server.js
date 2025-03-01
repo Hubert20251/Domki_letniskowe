f08ce4e3fc55f4fb77f3af6a55a1a821d3dcdf33
Hubert20251 <hubertziobrowski@gmail.com>
2025-03-01 23:21:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -907,7 +907,7 @@ app.delete('/api/bookings/:id', (req, res) => {
 
 // 📌 Uruchomienie serwera
 app.listen(3000, () => {
-    console.log('🚀 Serwer działa na http://localhost:3000');
+    console.log('🚀 Serwer działa na https://domki-letniskowe.onrender.com');
 });
 
 
